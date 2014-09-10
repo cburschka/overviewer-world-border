55ad1d7128d5f521634d7b6714300960fc3edbba
Christoph Burschka <christoph@burschka.de>
2014-09-10 14:40:11 +0200

    Hacked a checkbox into the overlay controls.

--- worldborder.js
+++ worldborder.js
@@ -3,9 +3,10 @@
 // @description This adds a world boundary overlay to the Overviewer map of the Spade server.
 // @namespace   http://ermarian.net/resources/minecraft/spade/
 // @include     http://te.calref.net/~minecraft/spade/*
-// @version     1.01
+// @version     2
 // @grant       none
 // ==/UserScript==
+
 circle = function (point, r, tileSet, opts, vertices) {
   opts.paths = [];
   for (var i = 0; i <= vertices; i++) {
@@ -15,23 +16,64 @@ circle = function (point, r, tileSet, opts, vertices) {
   }
   return new google.maps.Polygon(opts);
 }
+
+/* Copied from overviewer.views.OverlayControlView.addItem.
+ * The view and its DOM element are not exposed, so we need
+ * to find the element and apply this static function to it.
+ */
+overlayControlAddItem = function(control, item) {
+  var itemDiv = document.createElement('div');
+  var itemInput = document.createElement('input');
+  itemInput.type='checkbox';
+
+  itemInput.checked =false;
+
+  // give it a name
+  $(itemInput).attr("_mc_overlayname", item.name);
+  jQuery(itemInput).click((function(local_item) {
+    return function(e) {
+      item.action(local_item, e.target.checked);
+    };
+  })(item));
+
+  $(".dropDown", control)[0].appendChild(itemDiv);
+  itemDiv.appendChild(itemInput);
+  var textNode = document.createElement('text');
+  textNode.innerHTML = item.label + '<br/>';
+
+  itemDiv.appendChild(textNode);
+}
+
 overviewer.views.WorldBoundaryView = Backbone.View.extend({
   render: function () {
     if (overviewer.collections.spawnMarker) {
       var curTileSet = overviewer.mapView.options.currentTileSet;
       var spawn = curTileSet.get('spawn');
       var c = circle(spawn, 2000, curTileSet, {
-        'map': overviewer.map,
         strokeColor: '#FF0000',
         strokeWeight: 2,
         strokeOpacity: 0.5,
         fillColor: '#00FF00',
         fillOpacity: 0.25
       }, 100);
+      return c;
     }
   }
 });
+
 overviewer.util.ready(function () {
   var boundary = new overviewer.views.WorldBoundaryView();
-  boundary.render();
+  var circle = boundary.render();
+
+  // Find the overlay menu:
+  var overlayControl = overviewer.map.controls[google.maps.ControlPosition.TOP_RIGHT].j[2];
+
+  overlayControlAddItem(
+    overlayControl, {
+      label: "World Boundary",
+      name:"boundary",
+      action: function(item, checked) {
+        circle.setMap(checked ? overviewer.map : null);
+      }
+  });
 });
